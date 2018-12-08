fe1cbf4389099f581ceebeceb9343f0bd2c7eb98
Paul Wishart <misterwishart@gmail.com>
2018-12-08 14:29:01 +0000

    First draft of question model completed

--- src/models/question.js
+++ src/models/question.js
@@ -1,6 +1,84 @@
-re
+const RequestHelper = require('../helpers/request.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Question = function () {
+  this.question;
+  this.category;
+  this.correctAnswer;
+  this.answersArray;
+  this.playerID;
+  this.questionDifficulty = "medium";
+}
+
+Question.prototype.bindEvents = function () {
+
+  PubSub.subscribe('Player:question-category', (event) => {
+    const categoryObject = event.detail;
+    const apiCode = categoryObject.apiCode;
+    this.playerID = categoryObject.playerID;
+    const url = `https://opentdb.com/api.php?amount=1&category=${apiCode}&difficulty=medium&type=multiple`
+    const request = new RequestHelper(url);
+    request.get()
+      .then((data) => {this.addQuestionInfo(data.results)})
+      .then(() => {return this.setUpQuestion();})
+      .then((result) => {PubSub.publish("Question:question-ready", result);})
+      .catch((error) => {console.error(error);})
+  })
+
+  PubSub.subscribe('QuestionView:question-answered', (event) => {
+    const chosenAnswer = event.detail;
+    const result = this.checkAnswer(chosenAnswer);
+
+    if (result) {
+      PubSub.publish(`QuestionP${this.playerID}:answer-correct`, this.category);
+    }
+
+    PubSub.publish('Question:question-result', result);
+  })
+};
+
+Question.prototype.checkAnswer = function (chosenAnswer) {
+  return chosenAnswer === this.correctAnswer;
+};
+
+Question.prototype.addQuestionInfo = function (apiInfo) {
+  this.category = apiInfo.category;
+  this.question = apiInfo.question;
+  this.correctAnswer = apiInfo['correct_answer'];
+  this.answersArray = apiInfo['incorrect_answers'];
+  this.answersArray.push(this.correctAnswer);
+  // Need to randomise answers
+};
+
+Question.prototype.setUpQuestion = function () {
+  return {
+    question: this.question,
+    answers: this.answersArray
+  };
+};
+
+Question.prototype.randomiseAnswers = function () {
+  
+};
+
+module.exports = Question;
 
 /*
+
+{
+"category": "History",
+"type": "multiple",
+"difficulty": "medium",
+"question": "Which country had an &quot;Orange Revolution&quot; between 2004 and 2005?",
+"correct_answer": "Ukraine",
+"incorrect_answers": [
+"Belarus",
+"Latvia",
+"Lithuania"
+]
+}
+
+"https://opentdb.com/api.php?amount=1&category=23&difficulty=medium&type=multiple"
 
 require: RequestHelper
 
