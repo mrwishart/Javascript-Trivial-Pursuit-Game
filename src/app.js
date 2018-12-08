ddeb2e6e557247eae32ea6e5c8ef19007ba8a90c
Rob Wilson <rob.wilson195@gmail.com>
2018-12-08 13:00:49 +0000

    Worked on app and fixed some other pieces.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,30 @@
-const Dice = require("./models/dice.js")
+const RollView = require('./views/roll_view.js');
+const Dice = require("./models/dice.js");
+const Game = require('./models/game.js');
+const Player = require('./models/player.js');
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("Javascript loaded");
+
+  const rollView = new RollView();
+  rollView.bindEvents();
+
+  const dice = new Dice();
+  dice.bindEvents();
+
+  const player1 = new Player(1);
+  const player2 = new Player(2);
+  player1.bindEvents();
+  player2.bindEvents();
+
+
+
+
+
+
+
+  const game = new Game();
+  game.bindEvents();
+
 
 })
