20500ab913161bd1b014bfb42087c7d96619eefd
Rob Wilson <rob.wilson195@gmail.com>
2018-12-08 12:28:27 +0000

    Developed the rollView.

--- src/views/roll_view.js
+++ src/views/roll_view.js
@@ -1,18 +1,40 @@
-/*
-Instance Variables:
-- this.element
+const PubSub = require('../helpers/pub_sub.js');
 
-Methods:
+const RollView = function (element) {
+  this.element = element;
+}
 
-bindEvents
-  sub to Game:current-player - (receives: playerId)
-  renders display
-  waits for dice roll -> publishes 'RollView:dice-clicked' (sends: playerID)
-end
+RollView.prototype.bindEvents = function () {
+  PubSub.subsribe('Game:current-player', (evt) => {
+    const playerId = evt.detail;
+    this.render(playerId)
+  })
+};
 
-render
-  creates display (i.e some html/CSS stuf)
-  shows which player is playing
-  EXTENSION: Show what dice rolls
-end
-*/
+RollView.prototype.render = function (playerId) {
+  const parentElement = document.querySelecor('#display-view');
+
+  const rollContainer = document.createElement('div');
+  rollContainer.classList.add('roll-container');
+  parentElement.appendChild(rollContainer);
+
+  const instruction = document.createElement('p');
+  instruction.textContent = `Player ${playerId}, it's your turn!`;
+  rollContainer.appendChild(instruction);
+
+  const rollButton = document.createElement('button');
+  rollButton.textContent = 'Roll!'
+  rollContainer.appendChild(rollButton);
+
+  rollButton.addEventListener('click', (evt) => {
+    PubSub.subscribe('Player:roll-result', (evt) => {
+      const numRolled = evt.detail;
+      const numRolledElement = document.createElement('p');
+      numRolledElement.textContent = `${numRolled}`;
+      rollContainer.appendChild(numRolledElement);
+    });
+    PubSub.publish('RollView:dice-clicked', playerId);
+  });
+}
+
+  module.exports = RollView;
