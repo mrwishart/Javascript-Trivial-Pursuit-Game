9956d24d3c83add31d71749a3b9b373da0f88601
Ray <ray.greenan.89@outlook.com>
2018-12-08 14:24:46 +0000

    Board_View bindEvents and render complete

--- src/views/board_view.js
+++ src/views/board_view.js
@@ -1,27 +1,59 @@
+const PubSub = require('../helpers/pub_sub.js');
+const PieceView = require('../views/piece_view.js');
+
+const BoardView = function (element) {
+  this.element = element;
+}
+
+BoardView.prototype.bindEvents = function () {
+  PubSub.subscribe('Player:new-position', (evt) => {
+    const playerId = evt.detail.playerID;
+    const position = evt.detail.position;
+    const pie = this.pie;
+
+    this.element.innerHTML = '';
+    this.render(playerId, position, pie);
+  });
+};
+
+BoardView.prototype.render = function (playerId, position, pie) {
+  const oldPiece = document.querySelector(`#player-${playerId}-piece`);
+  oldPiece.parentNode.removeChild(oldPiece);
+
+  const htmlPosition = document.querySelector(`#box${position}`)
+
+  const pieceView = new PieceView(playerId, htmlPosition, pie);
+  pieceView.render();
+};
+
+
+//start positions
+module.exports = BoardView;
+
 /*
 require pieceView
 
 this.element = HTMLelement,
 
 bindEvents
-  setupStartPositions()
+setupStartPositions()
 
-  SUB Player:new-position, (playerID, pie, newPosition)
-    -remove old div
-    -render(playerID, pie, newPosition)
+SUB Player:new-position, (playerID, pie, newPosition)
+-remove old div
+-render(playerID, pie, newPosition)
 
 end
 
 render(playerID, pie, newPosition)
-  create new pieceView object (playerID, pie)
-  new pieceView.render (pieceView's render function)
-  append to newPosition
+create new pieceView object (playerID, pie)
+new pieceView.render (pieceView's render function)
+append to newPosition
 
 end
 
 setupStartPositions()
-  const emptyPiece = {categoryName: false,  ......}
-  render(1, emptyPiece, 0)
-  render(2, emptyPiece, 0)
+const emptyPiece = {categoryName: false,  ......}
+render(1, emptyPiece, 0)
+render(2, emptyPiece, 0)
 end
 */
