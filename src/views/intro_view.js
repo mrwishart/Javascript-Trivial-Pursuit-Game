b68c8f5023717eebd70878b09ac461e6538fe9a7
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-10 12:10:20 +0000

    refactored add player options to a reusable method

--- src/views/intro_view.js
+++ src/views/intro_view.js
@@ -15,35 +15,56 @@ IntroView.prototype.createIntroForm = function () {
   const introViewElement = document.createElement('div');
   introViewElement.id = 'win-view';
   this.parentElement.appendChild(introViewElement);
-
   const playerEntryForm = document.createElement('form');
+  this.addPlayerEntry('player 1', playerEntryForm);
+  this.addPlayerEntry('player 2', playerEntryForm);
+  this.addSubmit(playerEntryForm);
 
-  const playerOneLabel = document.createElement('label')
-  playerOneLabel.textContent = "Player One, enter your name:"
-  playerEntryForm.appendChild(playerOneLabel)
+  introViewElement.appendChild(playerEntryForm)
 
-  const playerOneEntry = document.createElement('input')
-  playerOneEntry.setAttribute('type', 'text')
-  playerOneEntry.setAttribute('value', 'player1')
-  playerEntryForm.appendChild(playerOneEntry)
+};
 
-  const playerTwoLabel = document.createElement('label')
-  playerTwoLabel.textContent = "Player Two, enter your name:"
-  playerEntryForm.appendChild(playerTwoLabel)
 
-  const playerTwoEntry = document.createElement('input')
-  playerTwoEntry.setAttribute('type', 'text')
-  playerTwoEntry.setAttribute('value', 'player2')
-  playerEntryForm.appendChild(playerTwoEntry)
+IntroView.prototype.addPlayerEntry = function (player, form) {
+  const label = document.createElement('label')
+  label.textContent = `${player}, enter your name:`
+  form.appendChild(label);
 
+  const entry = document.createElement('input')
+  entry.setAttribute('type', 'text')
+  entry.setAttribute('value', `${player}`)
+  form.appendChild(entry)
+};
+
+IntroView.prototype.addSubmit = function (form) {
   const introSubmit = document.createElement('input');
   introSubmit.setAttribute('type', 'submit');
   introSubmit.setAttribute('value', 'Start Game');
-  playerEntryForm.appendChild(introSubmit);
-
-
-  introViewElement.appendChild(playerEntryForm)
+  form.appendChild(introSubmit);
 };
+
+
+// const playerOneEntry = document.createElement('input')
+// playerOneEntry.setAttribute('type', 'text')
+// playerOneEntry.setAttribute('value', 'player1')
+// playerEntryForm.appendChild(playerOneEntry)
+//
+// const playerTwoLabel = document.createElement('label')
+// playerTwoLabel.textContent = "Player Two, enter your name:"
+// playerEntryForm.appendChild(playerTwoLabel)
+//
+// const playerTwoEntry = document.createElement('input')
+// playerTwoEntry.setAttribute('type', 'text')
+// playerTwoEntry.setAttribute('value', 'player2')
+// playerEntryForm.appendChild(playerTwoEntry)
+
+
+
+
+
+
+
+
 
 
 
