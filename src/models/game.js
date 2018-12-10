a3266316c80228376a65598549ce7d1646941d96
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-10 15:19:00 +0000

    can now display names, changed passTurn

--- src/models/game.js
+++ src/models/game.js
@@ -17,20 +17,25 @@ const Player = require('./player.js');
 const PubSub = require('../helpers/pub_sub.js');
 
 const Game = function (boardView, playerView) {
-  this.currentPlayer = 1;
+  this.currentPlayer = null;
+  this.currentPlayerID = 1;
   this.board = boardView;
   this.playerView = playerView;
+  this.players = [];
 };
 
 Game.prototype.bindEvents = function () {
   PubSub.subscribe('IntroForm:player-details-entered', (event) => {
     this.playerView.bindEvents();
     const playerObject = event.detail;
+    this.currentPlayer = {id: 1, name: playerObject[1]}
     for (playerID in playerObject){
       const newPlayer = new Player(playerID, playerObject[playerID]);
       newPlayer.bindEvents();
+      this.players.push({id: playerID, name: playerObject[playerID]})
     }
     this.board.setStartingPositions();
+    this.nextMove();
   })
 
   PubSub.subscribe('Question:question-result', (evt) => {
@@ -39,12 +44,13 @@ Game.prototype.bindEvents = function () {
     if (!answerCorrect) {this.passTurn()};
     this.nextMove();
   });
-  this.nextMove();
+
 };
 
 Game.prototype.passTurn = function () {
   const turnOrder = {1: 2, 2: 1};
-  this.currentPlayer = turnOrder[this.currentPlayer];
+  this.currentPlayerID = turnOrder[this.currentPlayer.id];
+  this.currentPlayer = this.players[this.currentPlayerID-1];
 };
 
 Game.prototype.nextMove = function () {
