7cf677e8916d412bd7662e85e497b9e6f9a4158d
Graeme Congreve <junkmailgmc@gmail.com>
2018-12-10 11:35:40 +0000

    added bindEvents to introView

--- src/views/intro_view.js
+++ src/views/intro_view.js
@@ -2,12 +2,21 @@ const PubSub = require('../helpers/pub_sub.js')
 
 const IntroView = function (parentElement) {
   this.parentElement = parentElement;
+  this.player1 = null;
+  this.player2 = null;
 }
 
 
 IntroView.prototype.bindEvents = function () {
   this.createIntroForm();
 };
+
+IntroView.prototype.createIntroForm = function () {
+  const introViewElement = document.createElement('div');
+  introViewElement.id = 'win-view';
+  this.parentElement.appendChild(introViewElement)
+};
+
 
 
 module.exports = IntroView;
